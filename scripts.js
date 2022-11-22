76cc605c920de1a4538064f2c438e9b547fda758
uwtrigeo <115599976+uwtrigeo@users.noreply.github.com>
2022-11-22 14:47:06 -0800

    final

---
+++ scripts.js
@@ -0,0 +1,46 @@
+// Initialize map
+var map = L.map('map').setView([50.10138520851064, -101.461714911189], 3);
+
+// Add basemap
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+}).addTo(map);
+
+var map2 = L.map('map2').setView([50.10138520851064, -101.461714911189], 3);
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+}).addTo(map2);
+
+// Read markers data from csv file
+$.get('data/ski_resort_stats.csv', function(csvString) {
+
+    // Use PapaParse to convert string to array of objects
+    var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
+
+    // For each row in data, create a marker and add it to the map
+    // For each row, columns `Latitude`, `Longitude`, and `Title` are required
+    for (var i in data) {
+      var row = data[i];
+
+      var marker = L.marker([row.lat, row.lon], {
+        opacity: 1
+      }).bindPopup(row.resort_name
+        );
+      
+      marker.addTo(map);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
